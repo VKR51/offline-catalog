db84c021bf497bdf11c3cd2565062cb6651d6e87
VKR51 <vvvvw51@gmail.com>
2026-04-17 10:13:37 +0300

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-const data = [
-{
-name: "Абрикос Чемпион Севера",
-description: "Небольшое дерево до 3 м...",
-images: [
-"https://raw.githubusercontent.com/VKR51/321321/main/Саженцы/Деревья/Абрикос%20Чемпион%20Севера/1.jpg"
-]
-}
-];
